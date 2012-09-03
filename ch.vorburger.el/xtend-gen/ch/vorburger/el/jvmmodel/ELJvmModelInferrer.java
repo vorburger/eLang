273ad771d4caff8b7816e3cbd978465304c383dd
Michael Vorburger <mike@vorburger.ch>
2012-09-03 12:46:31 +0200

    xtend/gen code as generated by Xtext 2.3.1

--- ch.vorburger.el/xtend-gen/ch/vorburger/el/jvmmodel/ELJvmModelInferrer.java
+++ ch.vorburger.el/xtend-gen/ch/vorburger/el/jvmmodel/ELJvmModelInferrer.java
@@ -1,8 +1,6 @@
 package ch.vorburger.el.jvmmodel;
 
-import java.util.Arrays;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.jvmmodel.AbstractModelInferrer;
 import org.eclipse.xtext.xbase.jvmmodel.IJvmDeclaredTypeAcceptor;
 
@@ -23,19 +21,13 @@ public class ELJvmModelInferrer extends AbstractModelInferrer {
    * @param isPreLinkingPhase - whether the method is called in a pre linking phase, i.e. when the global index isn't fully updated. You
    *        must not rely on linking using the index if iPrelinkingPhase is <code>true</code>
    */
-  protected void _infer(final XExpression element, final IJvmDeclaredTypeAcceptor acceptor, final boolean isPrelinkingPhase) {
+  public void _infer(final EObject element, final IJvmDeclaredTypeAcceptor acceptor, final boolean isPrelinkingPhase) {
   }
   
   public void infer(final EObject element, final IJvmDeclaredTypeAcceptor acceptor, final boolean isPrelinkingPhase) {
-    if (element instanceof XExpression) {
-      _infer((XExpression)element, acceptor, isPrelinkingPhase);
-      return;
-    } else if (element != null) {
+    {
       _infer(element, acceptor, isPrelinkingPhase);
       return;
-    } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(element, acceptor, isPrelinkingPhase).toString());
     }
   }
 }
