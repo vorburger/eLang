5bc9514ee196b5a498bf0febada7b0cad4035591
Michael Vorburger <mike@vorburger.ch>
2013-02-11 18:23:04 +0100

    Removing xtend-gen/ from Git (and adding to .gitignore)

--- ch.vorburger.el/xtend-gen/ch/vorburger/el/jvmmodel/ELIdentifiableSimpleNameProvider.java
+++
@@ -1,7 +0,0 @@
-package ch.vorburger.el.jvmmodel;
-
-import org.eclipse.xtext.xbase.featurecalls.IdentifiableSimpleNameProvider;
-
-@SuppressWarnings("all")
-public class ELIdentifiableSimpleNameProvider extends IdentifiableSimpleNameProvider {
-}
